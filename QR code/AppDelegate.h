32cd4188817ad68b8d022160a5e86319798087f6
OneBuzz <1906586363@qq.com>
2016-02-28 02:04:22 +0800

    仿微信二维码名片生成

---
+++ QR code/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QR code
+//
+//  Created by BuzzLightYear23 on 16/2/27.
+//  Copyright © 2016年 Irving. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QR code/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QR code
+//
+//  Created by BuzzLightYear23 on 16/2/27.
+//  Copyright © 2016年 Irving. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
